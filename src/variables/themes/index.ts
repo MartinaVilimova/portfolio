afad054071b087b3c5fb1a7f3cc8675870937e8e
Martina Vilimova <vilima.tul@gmail.com>
2021-02-23 20:59:16 +0100

    Add Light and Dark Theme context provider

---
+++ src/variables/themes/index.ts
@@ -0,0 +1,27 @@
+import { DefaultTheme } from 'styled-components'
+
+export const LightTheme: DefaultTheme = {
+	bg: {
+		primary: '#f1f3f6',
+		shadowWhite: '#fafafa',
+		shadowBlack: '#dadada',
+	},
+	text: {
+		primary: '#212529',
+		grey: '#8b9299',
+		green: '#2fb99e',
+	},
+}
+
+export const DarkTheme: DefaultTheme = {
+	bg: {
+		primary: '#2c2d30',
+		shadowWhite: '#3d3d3d',
+		shadowBlack: '#212225',
+	},
+	text: {
+		primary: '#fafafa',
+		grey: '#d7e2d2',
+		green: '#2fb99e',
+	},
+}
