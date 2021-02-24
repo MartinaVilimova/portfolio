6553f5bb69abbe218645a447c5d3a97eb7c29d7e
Martina Vilimova <vilima.tul@gmail.com>
2021-02-24 13:52:29 +0100

    Create SettingsBox

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,16 @@
 import React from 'react'
-import NavBar from '../features/NavBar'
 import Photo from '../components/Photo'
-import Toggle from '../components/Toggle'
-import { Theme, useThemeControl } from '../features/Theme'
+import NavBar from '../features/NavBar'
+import SettingsBox from '../features/Settings'
 
 const Home: React.FC = () => {
-	const [theme, setTheme] = useThemeControl()
 
 	return (
 		<>
 			<NavBar />
-			<Toggle
-				isChecked={theme === Theme.Dark}
-				onChange={(checked) =>
-					setTheme(checked ? Theme.Dark : Theme.Light)
-				}
-			/>
+			<h1>START</h1>
 			<Photo />
+			<SettingsBox />
 		</>
 	)
 }
