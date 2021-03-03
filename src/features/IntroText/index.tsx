0ae84547f838def8039475c92a8cd05e9ecd622e
Martina Vilimova <vilima.tul@gmail.com>
2021-03-03 22:03:12 +0100

    Add StyledParagraph and StyledH3

--- src/features/IntroText/index.tsx
+++ src/features/IntroText/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { ColorfulText } from '../../styled/ColorfulText/styled'
-import { Wrapper,  Subtitle } from './styled'
 import Typed from 'react-typed'
+import { ColorfulText } from '../../styled/ColorfulText/styled'
+import { Wrapper } from './styled'
 import { Button } from '../../components/Button/styled'
 import { StyledLink } from '../../styled/StyledLink/styled'
 import { StyledH1, StyledH4 } from '../../styled/StyledTitle/styled'
+import { Paragraph } from '../../styled/StyledParagraph/styled'
 
 const IntroText: React.FC = () => {
 	return (
@@ -29,11 +30,11 @@ const IntroText: React.FC = () => {
 					loop
 				></Typed>
 			</StyledH4>
-			<Subtitle>
+			<Paragraph>
 				Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris
 				dolor felis, sagittis at, luctus sed, aliquam non, tellus. Etiam
 				egestas wisi a erat. Mauris metus. Quisque porta.
-			</Subtitle>
+			</Paragraph>
 			<StyledLink to="/">
 				<Button>Stáhnout životopis</Button>
 			</StyledLink>
--- src/features/IntroText/styled.ts
+++ src/features/IntroText/styled.ts
@@ -4,20 +4,14 @@ import { MediaQueries } from '../../themes'
 export const Wrapper = styled.article`
 	display: flex;
 	justify-content: center;
+	align-items: center;
 	flex-direction: column;
 	width: 100%;
 	height: 100%;
 	box-sizing: border-box;
-	padding-right: 2em;
 
 	@media (max-width: ${MediaQueries.Ipad}) {
 		order: 2;
 		text-align: center;
-		padding-right: 0;
 	}
-`
-
-export const Subtitle = styled.p`
-	color: #8b9299;
-	padding-bottom: 0.7em;
 `
