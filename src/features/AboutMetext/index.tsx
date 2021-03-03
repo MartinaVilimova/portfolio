466284ab38028539fea4c961aa72d543a2a489b6
Martina Vilimova <vilima.tul@gmail.com>
2021-03-03 22:03:38 +0100

    Create AboutMeText

---
+++ src/features/AboutMetext/index.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { ColorfulText } from '../../styled/ColorfulText/styled'
+import {
+	Wrapper,
+	PersonalInfo1,
+	Name,
+	Text,
+	SmallText,
+	WrapperLinks,
+} from './styled'
+import { Button } from '../../components/Button/styled'
+import { StyledLink } from '../../styled/StyledLink/styled'
+import { StyledH3 } from '../../styled/StyledTitle/styled'
+import ContentColumns, {
+	NumberColumns,
+	NumberColumnsIpad,
+} from '../../components/ContentColumns'
+import { Paragraph } from '../../styled/StyledParagraph/styled'
+
+const AboutMeText: React.FC = () => (
+		<Wrapper>
+			<StyledH3>
+				<SmallText>Junior</SmallText>{' '}
+				<ColorfulText>Front-end</ColorfulText>
+			</StyledH3>
+			<ContentColumns
+				columns={NumberColumns.Columns2}
+				columnsIpad={NumberColumnsIpad.Columns2}
+			>
+				<PersonalInfo1>
+					<Name>
+						Narození:<Text> 14.&nbsp;7.&nbsp;1996</Text>
+					</Name>
+					<Name>
+						Nacházím se:<Text> Česká&nbsp;republika</Text>
+					</Name>
+					<Name>
+						Město:<Text> Praha</Text>
+					</Name>
+				</PersonalInfo1>
+				<PersonalInfo1>
+					<Name>
+						Telefon:<Text> +420&nbsp;728&nbsp;445&nbsp;052</Text>
+					</Name>
+					<Name>
+						Email:<Text> vilima.tul@gmail.com</Text>
+					</Name>
+					<Name>
+						Zaměstnaná:<Text> freelancer</Text>
+					</Name>
+				</PersonalInfo1>
+			</ContentColumns>
+			<Paragraph>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
+				aliquid eius repellat fugit sunt assumenda deleniti blanditiis!
+				Natus eius animi, voluptas, corrupti culpa iste ratione quam
+				deleniti, doloremque excepturi voluptatem. Lorem, ipsum dolor
+				sit amet consectetur adipisicing elit. Corrupti doloremque enim
+				reprehenderit minus similique illum mollitia ea architecto
+				placeat maiores, asperiores est harum atque animi omnis ipsum
+				quos obcaecati incidunt.
+			</Paragraph>
+			<WrapperLinks>
+				<StyledLink to="/">
+					<Button>Stáhnout životopis</Button>
+				</StyledLink>
+				<StyledLink to="/">
+					<Button>Napiš mi</Button>
+				</StyledLink>
+			</WrapperLinks>
+		</Wrapper>
+	)
+
+export default AboutMeText
---
+++ src/features/AboutMetext/styled.ts
@@ -0,0 +1,59 @@
+import styled from 'styled-components'
+import { MediaQueries } from '../../themes'
+
+export const Wrapper = styled.article`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 100%;
+	height: 100%;
+	padding-bottom: 1em;
+
+	& ul:nth-child(1) {
+	margin-left: 1.5em;
+	width: 90%;
+	}
+`
+
+export const SmallText = styled.span`
+	font-size: 0.7em;
+`
+
+export const PersonalInfo1 = styled.ul`
+	box-sizing: border-box;
+	width: 100%;
+	justify-self: center;
+	list-style: none;
+	padding: 0;
+
+	@media (max-width: ${MediaQueries.SmallerIpad}) {
+		width: 95%;
+	}
+`
+
+export const Name = styled.li`
+	font-size: 1em;
+	color: ${({ theme }) => theme.text.primary};
+	padding-bottom: 1em;
+`
+
+export const Text = styled.span`
+	display: block;
+	color: ${({ theme }) => theme.text.grey};
+	filter: brightness(70%);
+	overflow: hidden;
+
+	@media (max-width: ${MediaQueries.SmallerIpad}) {
+		font-size: 0.8em;
+	}
+`
+
+export const WrapperLinks = styled.div`
+	display: flex;
+	width: 80%;
+	justify-content: space-around;
+
+	@media (max-width: ${MediaQueries.SmallerIpad}) {
+		width: 100%;
+	}
+`
