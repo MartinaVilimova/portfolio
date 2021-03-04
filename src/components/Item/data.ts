3b7841928c48c7eda6a0c9b21024ad7ead6631ce
Martina Vilimova <vilima.tul@gmail.com>
2021-03-04 15:41:20 +0100

    Add animation to Item component

--- src/components/Item/data.ts
+++ src/components/Item/data.ts
@@ -1,6 +1,5 @@
 export type DataItems = {
 	title: string
-	id: string
 	time: string
 	paragraph: string
 }
@@ -8,14 +7,12 @@ export type DataItems = {
 export const dataEducation: DataItems[] = [
 	{
 		title: 'Technická univerzita Liberec',
-		id: '1',
 		time: '2015 - 2018',
 		paragraph:
 			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
 	},
 	{
 		title: 'Integrovaná střední škola živnostenská',
-		id: '2',
 		time: '2011 - 2015',
 		paragraph:
 			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur.',
@@ -25,7 +22,6 @@ export const dataEducation: DataItems[] = [
 export const dataExperience: DataItems[] = [
 	{
 		title: 'Vidia Design',
-		id: '3',
 		time: '2019',
 		paragraph:
 			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
@@ -35,63 +31,54 @@ export const dataExperience: DataItems[] = [
 export const dataKnowledge: DataItems[] = [
 	{
 		title: 'React Form',
-		id: '4',
 		time: 'Únor 2021',
 		paragraph:
 			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
 	},
 	{
 		title: 'React',
-		id: '5',
 		time: 'Leden 2021',
 		paragraph:
 			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
 	},
 	{
 		title: 'React Router Dom',
-		id: '6',
 		time: 'Leden 2021',
 		paragraph:
 			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
 	},
 	{
 		title: 'WordPress',
-		id: '7',
 		time: 'Říjen 2020',
 		paragraph:
 			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
 	},
 	{
 		title: 'Boostrap',
-		id: '8',
 		time: 'Září 2020',
 		paragraph:
 			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
 	},
 	{
 		title: 'Czechitas Web od A do Z 2',
-		id: '9',
 		time: 'Srpen 2020',
 		paragraph:
 			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
 	},
 	{
 		title: 'Vidia Design',
-		id: '10',
 		time: 'Listopad 2019',
 		paragraph:
 			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
 	},
 	{
 		title: 'CSS',
-		id: '11',
 		time: 'Červenec 2019',
 		paragraph:
 			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
 	},
 	{
 		title: 'Html',
-		id: '12',
 		time: 'Červenec 2019',
 		paragraph:
 			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
