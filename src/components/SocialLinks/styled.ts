2d98418c46c931e02891d525bff4af2852987236
Martina Vilimova <vilima.tul@gmail.com>
2021-03-07 19:49:32 +0100

    Add SocialLinks

---
+++ src/components/SocialLinks/styled.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+import { StyledA } from '../../styled/StyledLink/styled'
+
+export const Wrapper = styled.section`
+	display: flex;
+	justify-content: center;
+	width: 90%;
+`
+
+export const Icon = styled(StyledA)`
+	display: flex;
+	box-sizing: border-box;
+	width: 2.5em;
+	height: 2.5em;
+	padding: 0.5em;
+	margin: 0 1em 0.5em 1em;
+	border-radius: 50%;
+	color: ${({ theme }) => theme.text.colorful};
+	box-shadow: 0.3em 0.3em 0.3em ${({ theme }) => theme.bg.shadowBlack},
+		-0.3em -0.3em 0.3em ${({ theme }) => theme.bg.shadowWhite};
+
+	&:hover {
+		box-shadow: 0.3em 0.3em 0.3em ${({ theme }) => theme.bg.shadowBlack},
+			-0.3em -0.3em 0.3em ${({ theme }) => theme.bg.shadowWhite},
+			0.3em 0.3em 0.3em ${({ theme }) => theme.bg.shadowBlack} inset,
+			-0.3em -0.3em 0.3em ${({ theme }) => theme.bg.shadowWhite} inset;
+	}
+`
