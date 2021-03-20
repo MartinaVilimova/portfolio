49a0b8eb1919a40aafc99222a7fb8c227eb97ebb
Martina Vilimova <vilima.tul@gmail.com>
2021-03-20 17:59:44 +0100

    Add the display of the active button

---
+++ src/features/NavBar/Nav/data.ts
@@ -0,0 +1,27 @@
+export type MenuLinksType = {
+	link: string
+	name: string
+}
+
+export const menuLinks: MenuLinksType[] = [
+	{
+		link: '/about-me',
+		name: 'O mně',
+	},
+	{
+		link: '/resume',
+		name: 'Životopis',
+	},
+	{
+		link: '/knowledge',
+		name: 'Znalosti',
+	},
+	{
+		link: '/my-work',
+		name: 'Má práce',
+	},
+	{
+		link: '/contact',
+		name: 'Kontakt',
+	},
+]
