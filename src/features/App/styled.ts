d52a88db5094db8556566e97d750f00758af3b7d
Martina Vilimova <vilima.tul@gmail.com>
2021-03-23 14:27:36 +0100

    Add new design scrollbar in description (modal box)

--- src/features/App/styled.ts
+++ src/features/App/styled.ts
@@ -1,8 +1,11 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  
 	body {
-		box-sizing: border-box;
 		margin: 0;
     padding: 0;
 		font-family: 'Poppins', sans-serif;
