b5bcad7a0ea0e2a488f905f6eeefe7be4fd268b1
Tomas Hampl <tomas.hampl@qest.cz>
2021-04-18 15:25:17 +0200

    🌐 feat: update tranlation picker

--- src/features/Languages/index.tsx
+++ src/features/Languages/index.tsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react'
+import i18next from 'i18next'
+
 import { Wrapper, Language } from './styled'
+import { AvailableLanguages } from '../../locales'
 
-enum NamesLanguages {
-	CZ = 'cz',
-	EN = 'en',
-}
-
-const languageOptions = [NamesLanguages.CZ, NamesLanguages.EN]
+const languageOptions = [AvailableLanguages.CS, AvailableLanguages.EN]
 
 const Languages: React.FC = () => {
-	const [active, setActive] = useState(NamesLanguages.CZ)
+	const [active, setActive] = useState(i18next.language)
+	i18next.changeLanguage(active)
 
 	return (
 		<Wrapper>
