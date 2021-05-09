bf41d96d509b316d7b71707d183377fc3e84e254
Martina Vilimova <vilima.tul@gmail.com>
2021-05-09 11:01:21 +0200

    Translation - Knowledge page (part 1)

--- src/components/ItemResume/data.ts
+++ src/components/ItemResume/data.ts
@@ -35,65 +35,85 @@ export const getDataExperience = (
 	},
 ]
 
-export const getDataKnowledge: DataItemsResume[] = [
+export const getDataKnowledge = (
+	t: TFunction<'translation'>
+): DataItemsResume[] => [
 	{
-		title: 'React Hooks',
-		time: 'Duben 2021',
-		paragraph:
-			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
+		title: t('knowledge.knowledge13.title'),
+		specialization: t('knowledge.knowledge13.specialization'),
+		time: t('knowledge.knowledge13.time'),
+		paragraph: t('knowledge.knowledge13.paragraph'),
 	},
 	{
-		title: 'React Form',
-		time: 'Únor 2021',
-		paragraph:
-			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
+		title: t('knowledge.knowledge12.title'),
+		specialization: t('knowledge.knowledge12.specialization'),
+		time: t('knowledge.knowledge12.time'),
+		paragraph: t('knowledge.knowledge12.paragraph'),
 	},
 	{
-		title: 'React',
-		time: 'Leden 2021',
-		paragraph:
-			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
+		title: t('knowledge.knowledge11.title'),
+		specialization: t('knowledge.knowledge11.specialization'),
+		time: t('knowledge.knowledge11.time'),
+		paragraph: t('knowledge.knowledge11.paragraph'),
 	},
 	{
-		title: 'React Router Dom',
-		time: 'Leden 2021',
-		paragraph:
-			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
+		title: t('knowledge.knowledge10.title'),
+		specialization: t('knowledge.knowledge10.specialization'),
+		time: t('knowledge.knowledge10.time'),
+		paragraph: t('knowledge.knowledge10.paragraph'),
 	},
 	{
-		title: 'WordPress',
-		time: 'Říjen 2020',
-		paragraph:
-			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
+		title: t('knowledge.knowledge9.title'),
+		specialization: t('knowledge.knowledge9.specialization'),
+		time: t('knowledge.knowledge9.time'),
+		paragraph: t('knowledge.knowledge9.paragraph'),
 	},
 	{
-		title: 'Boostrap',
-		time: 'Září 2020',
-		paragraph:
-			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
+		title: t('knowledge.knowledge8.title'),
+		specialization: t('knowledge.knowledge8.specialization'),
+		time: t('knowledge.knowledge8.time'),
+		paragraph: t('knowledge.knowledge8.paragraph'),
 	},
 	{
-		title: 'Czechitas Web od A do Z 2',
-		time: 'Srpen 2020',
-		paragraph:
-			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
+		title: t('knowledge.knowledge7.title'),
+		specialization: t('knowledge.knowledge7.specialization'),
+		time: t('knowledge.knowledge7.time'),
+		paragraph: t('knowledge.knowledge7.paragraph'),
 	},
 	{
-		title: 'Vidia Design',
-		time: 'Listopad 2019',
-		paragraph:
-			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
+		title: t('knowledge.knowledge6.title'),
+		specialization: t('knowledge.knowledge6.specialization'),
+		time: t('knowledge.knowledge6.time'),
+		paragraph: t('knowledge.knowledge6.paragraph'),
 	},
 	{
-		title: 'CSS',
-		time: 'Červenec 2019',
-		paragraph:
-			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
+		title: t('knowledge.knowledge5.title'),
+		specialization: t('knowledge.knowledge5.specialization'),
+		time: t('knowledge.knowledge5.time'),
+		paragraph: t('knowledge.knowledge5.paragraph'),
 	},
 	{
-		title: 'Html',
-		time: 'Červenec 2019',
-		paragraph:
-			'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio nisi molestias officiis alias recusandae rem quos similique. Esse, pariatur. Architecto soluta perferendis eum? Accusantium similique laudantium quidem delectus non? Iusto.',
+		title: t('knowledge.knowledge4.title'),
+		specialization: t('knowledge.knowledge4.specialization'),
+		time: t('knowledge.knowledge4.time'),
+		paragraph: t('knowledge.knowledge4.paragraph'),
+	},
+	{
+		title: t('knowledge.knowledge3.title'),
+		specialization: t('knowledge.knowledge3.specialization'),
+		time: t('knowledge.knowledge3.time'),
+		paragraph: t('knowledge.knowledge3.paragraph'),
+	},
+	{
+		title: t('knowledge.knowledge2.title'),
+		specialization: t('knowledge.knowledge2.specialization'),
+		time: t('knowledge.knowledge2.time'),
+		paragraph: t('knowledge.knowledge2.paragraph'),
+	},
+	{
+		title: t('knowledge.knowledge1.title'),
+		specialization: t('knowledge.knowledge1.specialization'),
+		time: t('knowledge.knowledge1.time'),
+		paragraph: t('knowledge.knowledge1.paragraph'),
 	},
 ]
--- src/contentPage/Knowledge/index.tsx
+++ src/contentPage/Knowledge/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import { Link } from 'react-scroll'
 import ItemResume, { AnimationType } from 'components/ItemResume'
 import { getDataKnowledge } from 'components/ItemResume/data'
@@ -7,31 +8,40 @@ import { StyledA } from 'styled/StyledLink/styled'
 import { Paragraph } from 'styled/StyledParagraph/styled'
 import { BlockAnchors } from './styled'
 
-const KnowledgeContent: React.FC = () => (
-	<>
-		<StyledH3>S čím jsem se na cestě IT setkala</StyledH3>
-		<Paragraph>
-			Nemám počítačovou školu ani žádné podobné vzdělání v tomto odvětí.
-			Přesto mě tento obor natolik nadchl, že jsem se rozhodla tomu dát
-			šanci. Začala jsem základy a postupně na to nabalovala další
-			znalosti. Učila jsem se z informací získaných na internetu, zašla na
-			pár <StyledA href="https://www.czechitas.cz/cs/">kurzů</StyledA>,
-			dělala projekty s přáteli nebo jsem si hrála&nbsp;
-			<StyledA href="https://codecombat.com/play">CodeCombat</StyledA>.
-		</Paragraph>
-		<BlockAnchors>
-			<StyledH6>Co jsem se naučila</StyledH6>
-			{getDataKnowledge.map((text, index) => (
-				<Link key={index} to={text.title} smooth={true} offset={-85}>
-					{text.title}
-				</Link>
-			))}
-		</BlockAnchors>
-		<ItemResume
-			data={getDataKnowledge}
-			animationType={AnimationType.Fade}
-		/>
-	</>
-)
+const KnowledgeContent: React.FC = () => {
+	const { t } = useTranslation()
+	const knowledge = getDataKnowledge(t)
+
+	return (
+		<>
+			<StyledH3>{t('knowledge.title')}</StyledH3>
+			<Paragraph>
+				Nemám počítačovou školu ani žádné podobné vzdělání v tomto
+				odvětí. Přesto mě tento obor natolik nadchl, že jsem se rozhodla
+				tomu dát šanci. Začala jsem základy a postupně na to nabalovala
+				další znalosti. Učila jsem se z informací získaných na
+				internetu, zašla na pár{' '}
+				<StyledA href="https://www.czechitas.cz/cs/">kurzů</StyledA>,
+				dělala projekty s přáteli nebo jsem si hrála&nbsp;
+				<StyledA href="https://codecombat.com/play">CodeCombat</StyledA>
+				.
+			</Paragraph>
+			<BlockAnchors>
+				<StyledH6>{t('knowledge.titleAnchor')}</StyledH6>
+				{knowledge.map((text, index) => (
+					<Link
+						key={index}
+						to={text.title}
+						smooth={true}
+						offset={-85}
+					>
+						{text.title}
+					</Link>
+				))}
+			</BlockAnchors>
+			<ItemResume data={knowledge} animationType={AnimationType.Fade} />
+		</>
+	)
+}
 
 export default KnowledgeContent
