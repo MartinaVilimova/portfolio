b1d7fb73986cfea21c6fb6c491fc889a5310c944
Martina Vilimova <vilima.tul@gmail.com>
2021-05-12 19:31:51 +0200

    Translation - Knowledge page (part 2)

--- src/contentPage/Knowledge/index.tsx
+++ src/contentPage/Knowledge/index.tsx
@@ -4,7 +4,6 @@ import { Link } from 'react-scroll'
 import ItemResume, { AnimationType } from 'components/ItemResume'
 import { getDataKnowledge } from 'components/ItemResume/data'
 import { StyledH3, StyledH6 } from 'styled/StyledTitle/styled'
-import { StyledA } from 'styled/StyledLink/styled'
 import { Paragraph } from 'styled/StyledParagraph/styled'
 import { BlockAnchors } from './styled'
 
@@ -15,17 +14,7 @@ const KnowledgeContent: React.FC = () => {
 	return (
 		<>
 			<StyledH3>{t('knowledge.title')}</StyledH3>
-			<Paragraph>
-				Nemám počítačovou školu ani žádné podobné vzdělání v tomto
-				odvětí. Přesto mě tento obor natolik nadchl, že jsem se rozhodla
-				tomu dát šanci. Začala jsem základy a postupně na to nabalovala
-				další znalosti. Učila jsem se z informací získaných na
-				internetu, zašla na pár{' '}
-				<StyledA href="https://www.czechitas.cz/cs/">kurzů</StyledA>,
-				dělala projekty s přáteli nebo jsem si hrála&nbsp;
-				<StyledA href="https://codecombat.com/play">CodeCombat</StyledA>
-				.
-			</Paragraph>
+			<Paragraph>{t('knowledge.subtitle')}</Paragraph>
 			<BlockAnchors>
 				<StyledH6>{t('knowledge.titleAnchor')}</StyledH6>
 				{knowledge.map((text, index) => (
