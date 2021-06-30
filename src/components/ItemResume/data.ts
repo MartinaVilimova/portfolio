621b8d389f9b53958cd6d290e0656cdf384d118b
Martina Vilimova <vilima.tul@gmail.com>
2021-06-30 22:56:19 +0200

    Add list of used programs

--- src/components/ItemResume/data.ts
+++ src/components/ItemResume/data.ts
@@ -1,10 +1,19 @@
 import { TFunction } from 'react-i18next'
 
+export enum TitleType {
+	program = 'program',
+	code = 'code',
+	education = 'education',
+	experience = 'experience',
+}
+
 export type DataItemsResume = {
 	title: string
 	specialization?: string
-	time: string
-	paragraph: string
+	time?: string
+	paragraph?: string
+	type: TitleType
+	isVisible?: boolean
 }
 
 export const getDataEducation = (
@@ -15,12 +24,14 @@ export const getDataEducation = (
 		specialization: t('resume.education2.specialization'),
 		time: t('resume.education2.time'),
 		paragraph: t('resume.education2.paragraph'),
+		type: TitleType.education,
 	},
 	{
 		title: t('resume.education1.title'),
 		specialization: t('resume.education1.specialization'),
 		time: t('resume.education1.time'),
 		paragraph: t('resume.education1.paragraph'),
+		type: TitleType.education,
 	},
 ]
 
@@ -32,6 +43,7 @@ export const getDataExperience = (
 		specialization: t('resume.experience1.specialization'),
 		time: t('resume.experience1.time'),
 		paragraph: t('resume.experience1.paragraph'),
+		type: TitleType.experience,
 	},
 ]
 
@@ -39,81 +51,143 @@ export const getDataKnowledge = (
 	t: TFunction<'translation'>
 ): DataItemsResume[] => [
 	{
+		title: t('knowledge.knowledge19.title'),
+		specialization: t('knowledge.knowledge19.specialization'),
+		time: t('knowledge.knowledge19.time'),
+		paragraph: t('knowledge.knowledge19.paragraph'),
+		type: TitleType.program,
+		isVisible: true,
+	},
+	{
+		title: t('knowledge.knowledge18.title'),
+		specialization: t('knowledge.knowledge18.specialization'),
+		time: t('knowledge.knowledge18.time'),
+		paragraph: t('knowledge.knowledge18.paragraph'),
+		type: TitleType.code,
+		isVisible: true,
+	},
+	{
+		title: t('knowledge.knowledge17.title'),
+		specialization: t('knowledge.knowledge17.specialization'),
+		time: t('knowledge.knowledge17.time'),
+		paragraph: t('knowledge.knowledge17.paragraph'),
+		type: TitleType.code,
+		isVisible: true,
+	},
+	{
+		title: t('knowledge.knowledge16.title'),
+		specialization: t('knowledge.knowledge16.specialization'),
+		time: t('knowledge.knowledge16.time'),
+		paragraph: t('knowledge.knowledge16.paragraph'),
+		type: TitleType.code,
+		isVisible: true,
+	},
+	{
+		title: t('knowledge.knowledge15.title'),
+		specialization: t('knowledge.knowledge15.specialization'),
+		time: t('knowledge.knowledge15.time'),
+		paragraph: t('knowledge.knowledge15.paragraph'),
+		type: TitleType.program,
+		isVisible: true,
+	},
+	{
+		title: t('knowledge.knowledge14.title'),
+		specialization: t('knowledge.knowledge14.specialization'),
+		time: t('knowledge.knowledge14.time'),
+		paragraph: t('knowledge.knowledge14.paragraph'),
+		type: TitleType.program,
+		isVisible: true,
+	},
+	{
 		title: t('knowledge.knowledge13.title'),
 		specialization: t('knowledge.knowledge13.specialization'),
 		time: t('knowledge.knowledge13.time'),
 		paragraph: t('knowledge.knowledge13.paragraph'),
+		type: TitleType.code,
+		isVisible: true,
 	},
 	{
 		title: t('knowledge.knowledge12.title'),
 		specialization: t('knowledge.knowledge12.specialization'),
 		time: t('knowledge.knowledge12.time'),
 		paragraph: t('knowledge.knowledge12.paragraph'),
+		type: TitleType.code,
+		isVisible: true,
 	},
 	{
 		title: t('knowledge.knowledge11.title'),
 		specialization: t('knowledge.knowledge11.specialization'),
 		time: t('knowledge.knowledge11.time'),
 		paragraph: t('knowledge.knowledge11.paragraph'),
+		type: TitleType.code,
+		isVisible: true,
 	},
 	{
 		title: t('knowledge.knowledge10.title'),
-		specialization: t('knowledge.knowledge10.specialization'),
-		time: t('knowledge.knowledge10.time'),
-		paragraph: t('knowledge.knowledge10.paragraph'),
+		type: TitleType.program,
+		isVisible: false,
 	},
 	{
 		title: t('knowledge.knowledge9.title'),
 		specialization: t('knowledge.knowledge9.specialization'),
 		time: t('knowledge.knowledge9.time'),
 		paragraph: t('knowledge.knowledge9.paragraph'),
+		type: TitleType.code,
+		isVisible: true,
 	},
 	{
 		title: t('knowledge.knowledge8.title'),
 		specialization: t('knowledge.knowledge8.specialization'),
 		time: t('knowledge.knowledge8.time'),
 		paragraph: t('knowledge.knowledge8.paragraph'),
+		type: TitleType.code,
+		isVisible: true,
 	},
 	{
 		title: t('knowledge.knowledge7.title'),
-		specialization: t('knowledge.knowledge7.specialization'),
-		time: t('knowledge.knowledge7.time'),
-		paragraph: t('knowledge.knowledge7.paragraph'),
+		type: TitleType.program,
+		isVisible: false,
 	},
 	{
 		title: t('knowledge.knowledge6.title'),
-		specialization: t('knowledge.knowledge6.specialization'),
-		time: t('knowledge.knowledge6.time'),
-		paragraph: t('knowledge.knowledge6.paragraph'),
+		type: TitleType.program,
+		isVisible: false,
 	},
 	{
 		title: t('knowledge.knowledge5.title'),
 		specialization: t('knowledge.knowledge5.specialization'),
 		time: t('knowledge.knowledge5.time'),
 		paragraph: t('knowledge.knowledge5.paragraph'),
+		type: TitleType.code,
+		isVisible: true,
 	},
 	{
 		title: t('knowledge.knowledge4.title'),
 		specialization: t('knowledge.knowledge4.specialization'),
 		time: t('knowledge.knowledge4.time'),
 		paragraph: t('knowledge.knowledge4.paragraph'),
+		type: TitleType.code,
+		isVisible: true,
 	},
 	{
 		title: t('knowledge.knowledge3.title'),
 		specialization: t('knowledge.knowledge3.specialization'),
 		time: t('knowledge.knowledge3.time'),
 		paragraph: t('knowledge.knowledge3.paragraph'),
+		type: TitleType.code,
+		isVisible: true,
 	},
 	{
 		title: t('knowledge.knowledge2.title'),
-		specialization: t('knowledge.knowledge2.specialization'),
-		time: t('knowledge.knowledge2.time'),
-		paragraph: t('knowledge.knowledge2.paragraph'),
+		type: TitleType.program,
+		isVisible: false,
 	},
 	{
 		title: t('knowledge.knowledge1.title'),
 		specialization: t('knowledge.knowledge1.specialization'),
 		time: t('knowledge.knowledge1.time'),
 		paragraph: t('knowledge.knowledge1.paragraph'),
+		type: TitleType.program,
+		isVisible: true,
 	},
 ]
--- src/contentPage/Knowledge/index.tsx
+++ src/contentPage/Knowledge/index.tsx
@@ -2,33 +2,59 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-scroll'
 import ItemResume, { AnimationType } from 'components/ItemResume'
-import { getDataKnowledge } from 'components/ItemResume/data'
+import { getDataKnowledge, TitleType } from 'components/ItemResume/data'
 import { StyledH3, StyledH6 } from 'styled/StyledTitle/styled'
 import { Paragraph } from 'styled/StyledParagraph/styled'
 import { BlockAnchors } from './styled'
 
 const KnowledgeContent: React.FC = () => {
 	const { t } = useTranslation()
-	const knowledge = getDataKnowledge(t)
+	const allKnowledge = getDataKnowledge(t)
 
 	return (
 		<>
 			<StyledH3>{t('knowledge.title')}</StyledH3>
 			<Paragraph>{t('knowledge.subtitle')}</Paragraph>
 			<BlockAnchors>
-				<StyledH6>{t('knowledge.titleAnchor')}</StyledH6>
-				{knowledge.map((text, index) => (
-					<Link
-						key={index}
-						to={text.title}
-						smooth={true}
-						offset={-85}
-					>
-						{text.title}
-					</Link>
-				))}
+				<StyledH6>{t('knowledge.titleProgram')}</StyledH6>
+				{allKnowledge
+					.filter((knowledge) => knowledge.type === TitleType.program)
+					.map((text, index) =>
+						!text.isVisible ? (
+							<span key={index}>{text.title}</span>
+						) : (
+							<Link
+								key={index}
+								to={text.title}
+								smooth={true}
+								offset={-85}
+							>
+								{text.title}
+							</Link>
+						)
+					)}
 			</BlockAnchors>
-			<ItemResume data={knowledge} animationType={AnimationType.Fade} />
+			<BlockAnchors>
+				<StyledH6>{t('knowledge.titleAnchor')}</StyledH6>
+				{allKnowledge
+					.filter((knowledge) => knowledge.type === TitleType.code)
+					.map((text, index) => (
+						<Link
+							key={index}
+							to={text.title}
+							smooth={true}
+							offset={-85}
+						>
+							{text.title}
+						</Link>
+					))}
+			</BlockAnchors>
+			<ItemResume
+				data={allKnowledge.filter(
+					(knowledge) => knowledge.isVisible === true
+				)}
+				animationType={AnimationType.Fade}
+			/>
 		</>
 	)
 }
